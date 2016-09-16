c03c4a6190cbf1d9c5cc4ccddc2fa4dca929fb3d
prashanth8055 <tpra16ec@cmrit.ac.in>
2016-09-16 13:01:29 +0530

    Update main.c

--- labprog1/main.c
+++ labprog1/main.c
@@ -1,1 +1,27 @@
-rti0
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+  float a,b,c,d,r1,r2,rr,ir;
+  printf("enter the cofficents of the quadratic equation\n");
+  scanf("%f%f%f",&a,&b,&c);
+  d= b*b-4*a*c;
+  if (d==0)
+     {
+        r1=r2=-b/(2*a);
+        printf(" the roots are real and equal r1=r2=%f",r1);
+     }
+        if (d>0)
+           {
+              r1=(-b+sqrt(d))/(2*a);
+              r2=(-b-sqrt(d))/(2*a);
+              printf(" the roots are real and different r1=%f,r2=%f\n",r1,r2);
+           }
+              else 
+                {
+                   rr=(-b)/(2*a);
+                   ir=(sqrt(-d))/(2*a);
+                   printf(" the roots are complex and imaginary r1=%f+i%f,r2=%f+i%f\n",rr,ir,rr,ir);
+                }
+    return 0;
+}
